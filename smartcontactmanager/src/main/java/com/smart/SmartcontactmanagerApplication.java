1b46f590392f68255f5449a825422f17a5b6ab9e
Bhubanananda2020 <bkhadi0203@gmail.com>
2021-11-07 14:41:00 +0530

    jtw R1

--- smartcontactmanager/src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ smartcontactmanager/src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class SmartcontactmanagerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartcontactmanagerApplication.class, args);
-	}
 
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	            return application.sources(SmartcontactmanagerApplication.class); // Replace DemoApplication with your main class
 	    }
+
+		public static void main(String[] args) {
+			SpringApplication.run(SmartcontactmanagerApplication.class, args);
+		}
+
+
+
+
 }
